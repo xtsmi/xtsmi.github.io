49585c30b57f1b792291cafc0ce1500caad24e46
tabuna <bliz48rus@gmail.com>
2020-07-03 19:22:29 +0300

    Date indication

--- app/Console/Commands/RssParseCommand.php
+++ app/Console/Commands/RssParseCommand.php
@@ -93,7 +93,9 @@ class RssParseCommand extends Command
             ->map(function (Collection $items) {
 
                 $fresh = $items->filter(function (News $news){
-                    return now()->subHours(8)->isAfter($news->pubDate);
+                    return Carbon::parse($news->pubDate)
+                        ->addHours(8)
+                        ->isAfter(now());
                 });
 
                 return $fresh->count() > 5 ? $fresh : $items;
@@ -121,7 +123,7 @@ class RssParseCommand extends Command
     {
         $pubDate = Carbon::parse((string)$item->pubDate);
 
-        if (now()->subDay()->isAfter($pubDate)) {
+        if ($pubDate->addDay()->isAfter(now())) {
             return null;
         }
 
