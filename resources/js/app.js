fb508aa51dd8734338b886092859af0dc17fb447
funtom48 <ivan40in48@gmail.com>
2020-07-06 23:11:57 +0300

    :boom: eslint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,14 @@
-require('./bootstrap');
+import './bootstrap';
 
 import Turbolinks from "turbolinks"
 
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-
+ 
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
-
+    application.load(definitionsFromContext(context))
+    
 Turbolinks.start()
