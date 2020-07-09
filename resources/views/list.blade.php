9c181bf2dce46bb7528c00530116459fedfb7c94
funtom48 <ivan40in48@gmail.com>
2020-07-10 01:09:03 +0300

    :sparkles: infinite scroll

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,23 +3,14 @@
 @section('content')
     <div data-controller="list">
         @foreach($news->take(10) as $new)
-            <div class="col mb-5">
-
-                <div class="media v-center mb-2">
-                    <img src="{{ $new->favicon() }}" class="mr-2">
-                    <div class="media-body">{{ $new->domain() }}</div>
-                </div>
-                <h5>
-                    <a href="{{ $new->link }}" target="_blank" rel="noopener noreferrer"
-                    class="text-dark">{{ $new->title }}</a>
-                </h5>
-
-                <time class="mr-1 text-muted small" datetime=" {{ $new->pubDate }}">
-                    {{ $new->pubDate }}
-                </time>
-            </div>
+            @include('particles.list_item', [
+                'new' => $new
+            ])
         @endforeach
     </div>
+    <template id='newsListItemTemplate'>
+        @include('particles.list_item', ['new' => []])
+    </template>
 
 
 @endsection
