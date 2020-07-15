fabebfc8ff4f035fe5de623936fd27d2f1161a50
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-07-15 11:31:33 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -79,14 +79,14 @@
     <footer class="footer mt-auto py-3">
 
         <div class="row">
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-4">
                 <img class="mb-2" height="50px" src="/img/logo2.svg" alt="TSMI">
                 <p class="text-muted">
                     Актуальные новости из различных источников.
                 </p>
                 <small class="d-block mb-3 text-muted">&copy; 2017-{{ date('Y') }}</small>
             </div>
-            <div class="col-6 col-md">
+            <div class="col-6 col-md-2">
                 <h5>About</h5>
                 <ul class="list-unstyled text-small">
                     <li><a class="text-muted" href="#">Team</a></li>
@@ -94,7 +94,7 @@
                     <li><a class="text-muted" href="#">Privacy</a></li>
                 </ul>
             </div>
-            <div class="col-6 col-md">
+            <div class="col-6 col-md-2">
                 <h5>Resources</h5>
                 <ul class="list-unstyled text-small">
                     <li><a class="text-muted" href="#">Resource</a></li>
@@ -103,7 +103,7 @@
                 </ul>
             </div>
             <div class="col-6 col-md">
-                <p class="text-right">
+                <p>
                     <small class="border p-2 rounded"> 18+ </small>
                 </p>
             </div>
