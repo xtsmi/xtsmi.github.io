325db30a738f76ee78ff52f0ef36f96622bd9c97
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-07-20 01:14:14 +0300

    Update one_news_controller.js

--- resources/js/controllers/one_news_controller.js
+++ resources/js/controllers/one_news_controller.js
@@ -2,7 +2,7 @@ import {Controller} from 'stimulus';
 
 export default class extends Controller {
 
-    connect() {
+    initialize() {
         const baseURI = document.baseURI;
 
         if (document.referrer.includes(baseURI)) {
