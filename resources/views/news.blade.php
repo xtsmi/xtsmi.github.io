bcabfc5b8c01e807b395856c9569fcdd17f32444
tabuna <bliz48rus@gmail.com>
2020-07-22 13:40:12 +0300

    refs #4 Redirect to source

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,25 +6,22 @@
 
 @section('content')
 
+    <main data-controller="one-news"
+          data-one-news-url="{{ $news->link }}">
 
-    @empty($story)
-        <div data-controller="one-news"
-             data-one-news-url="{{ $news->link }}">
+        @empty($story)
             <x-news :news="$news" direct="true"/>
-        </div>
+        @else
+            <article class="bg-white pt-3 px-3 mb-4 rounded shadow-sm">
+                <x-story :story="$story"/>
 
-    @else
-
-        <article class="bg-white pt-3 px-3 mb-4 rounded shadow-sm">
-            <x-story :story="$story"/>
-
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-                @foreach($story->get('items') as $news)
-                    <x-news :news="$news"/>
-                @endforeach
-            </div>
-        </article>
-
-    @endempty
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+                    @foreach($story->get('items') as $news)
+                        <x-news :news="$news"/>
+                    @endforeach
+                </div>
+            </article>
+        @endempty
+    </main>
 
 @endsection
