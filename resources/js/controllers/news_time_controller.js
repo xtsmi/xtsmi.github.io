d8b1c8a1505db12f63e2270c4b12459cdc5ea332
funtom48 <ivan40in48@gmail.com>
2020-07-27 21:20:40 +0300

    :bug: fix time element

--- resources/js/controllers/news_time_controller.js
+++ resources/js/controllers/news_time_controller.js
@@ -3,6 +3,6 @@ import { timeSince } from '../lib/format_date';
 
 export default class extends Controller {
     initialize() {
-        this.element.innerHTML = timeSince(this.element.innerText);
+        this.element.innerHTML = timeSince(this.element.innerText.trim());
     }
 }
