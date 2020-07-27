96f0d04d598d7f00255c167a7a3d8616099f3ce8
funtom48 <ivan40in48@gmail.com>
2020-07-27 23:56:41 +0300

    :recycle: refactiring story items

--- resources/js/controllers/story_items_controller.js
+++ resources/js/controllers/story_items_controller.js
@@ -1,16 +1,13 @@
-import {Controller} from 'stimulus';
+import { Controller } from 'stimulus';
 
 export default class extends Controller {
-
     show(event) {
+        const items = this.element.getElementsByClassName('d-none');
 
-        let items = this.element.getElementsByClassName('d-none');
-
-        for (let i = 0; i < items.length; i++) {
-            items[i].classList.remove("d-none");
+        for (const item of items) {
+            item.classList.remove('d-none');
         }
 
-        event.target.parentElement.classList.add("d-none")
+        event.target.parentElement.classList.add('d-none');
     }
-
 }
