9516c25dda7c33e36b6bd2142fb2161dddb5e08b
tabuna <bliz48rus@gmail.com>
2020-08-03 11:09:39 +0300

    Added keywords and spare key

--- app/News.php
+++ app/News.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Carbon\Carbon;
+use DonatelloZa\RakePlus\RakePlus;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use Spatie\Feed\Feedable;
 use Spatie\Feed\FeedItem;
 use Symfony\Component\Mime\MimeTypes;
@@ -35,6 +38,7 @@ class News extends Model implements Feedable
         'domain',
         'image',
         'internalLink',
+        'keywords',
     ];
 
     /**
@@ -49,6 +53,7 @@ class News extends Model implements Feedable
         'domain'      => 'string',
         'favicon'     => 'string',
         'image'       => 'string',
+        'keywords'    => 'array',
     ];
 
     /**
@@ -128,6 +133,37 @@ class News extends Model implements Feedable
     }
 
     /**
+     * @return array
+     */
+    public function getKeywordsAttribute(): array
+    {
+        $description = Str::before(strip_tags($this->description ?? $this->title), '.');
+
+        return (new Stringable($description))
+            ->explode(' ')
+            ->map(static function (string $word) {
+
+                $word = str_replace(['(', ')', '"', "'", '$', '«'], '', $word);
+
+                if (is_numeric($word) || mb_strlen($word) < 3) {
+                    return null;
+                }
+
+                return mb_convert_case($word, MB_CASE_TITLE, 'UTF-8') === $word
+                    ? $word
+                    : null;
+            })
+            ->filter()
+            ->skip(1)
+            ->whenEmpty(function () use ($description) {
+                $rake = RakePlus::create($description, 'ru_RU', 3);
+
+                return collect(Arr::last($rake->keywords()));
+            })
+            ->toArray();
+    }
+
+    /**
      * @return array|FeedItem
      */
     public function toFeedItem()
@@ -135,12 +171,20 @@ class News extends Model implements Feedable
         $image = $this->image ?? url('/img/cover.jpg');
         $mimeTypes = (new MimeTypes())->getMimeTypes(pathinfo($image, PATHINFO_EXTENSION));
 
+
+        $summary = Str::before(strip_tags($this->description ?? $this->title), '.');
+        $keywords = $this->keywords;
+        $keywordsWithHahTag = array_map(function (string $word) {
+            return '#' . $word;
+        }, $keywords);
+
+        $summary = str_replace($keywords, $keywordsWithHahTag, $summary);
+
+
         return FeedItem::create()
             ->id(Str::slug($this->pubDate . '/' . $this->id))
             ->title($this->title)
-            ->summary(
-                Str::before(strip_tags($this->description ?? $this->title), '.')
-            )
+            ->summary($summary)
             ->enclosure($image)
             ->enclosureType(array_shift($mimeTypes) ?? 'image/unknown')
             ->enclosureLength(0)
