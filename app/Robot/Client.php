5e3533323402b0cc897f42dd66dce2449ed48003
tabuna <bliz48rus@gmail.com>
2021-01-23 02:07:52 +0300

    Timeout increment 20

--- app/Robot/Client.php
+++ app/Robot/Client.php
@@ -19,7 +19,7 @@ class Client
      */
     public $options = [
         'verify'          => false,
-        'timeout'         => 10.0,
+        'timeout'         => 20.0,
         'http_errors'     => false,
         'headers'         => [
             'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
