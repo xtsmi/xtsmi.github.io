d29c5ca24cf2309ba2ffa863a0af706f74031b15
tabuna <bliz48rus@gmail.com>
2021-05-06 19:35:33 +0300

    Update cs-fixer

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -18,7 +18,7 @@ return (new PhpCsFixer\Config())
         'no_unused_imports' => true,
         'no_whitespace_before_comma_in_array' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => ['align_double_arrow' => true, 'align_equals' => false],
